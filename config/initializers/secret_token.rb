dd66599c70c18d27214a9994070a3b658a3e35ef
KOOSHKA <luis.kooshka@gmail.com>
2011-02-11 21:15:20 +0000

    Signed-off-by: KOOSHKA <luis.kooshka@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5ba929119f36f623ad0add008385859ef139e7ca62075f67d2ca7185959929c94187b1fa32195c87004aba43890ee9fd17569496171b5b9ffe33dfcab9bc872d'
